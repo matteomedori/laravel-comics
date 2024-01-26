233b067689622378ab992efda253fecd73544336
matteomedori <matteo.medori.97@gmail.com>
2024-01-26 17:29:47 +0100

    feat: footer first commit

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,114 @@
-<footer>footer</footer>
+<footer>
+    {{-- footer up --}}
+    <div class="footer-up">
+        <div class="container">
+            <div class="left">
+                <div class="col">
+                    <h4>DC Comics</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                    </ul>
+                    <h4>Shop</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h4>Sites</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="logo-bg" />
+        </div>
+    </div>
+    {{-- /footer up --}}
+
+    {{-- footer down --}}
+    <div class="footer-down">
+        <div class="container">
+            <button>Sign up now!</button>
+            <ul>
+                <li>Follow us</li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook-footer" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter-footer" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube-footer" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope-footer" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest-footer" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    {{-- /footer down --}}
+</footer>
